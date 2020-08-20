a2e9e9dbcc92b6643c6b15b001b3a46124b8aaba
David Lambert <codepenguin@gmail.com>
2020-08-19 22:10:38 -0400

    Refactored buttons to an inherited base class to handle the visible aspects.

---
+++ ShortDash.Server/Components/DashboardGridButtonBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShortDash.Server.Data;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace ShortDash.Server.Components
+{
+    public abstract class DashboardButtonBase : ComponentBase
+    {
+        [Parameter]
+        public GridCell Cell { get; set; }
+
+        protected Dictionary<string, object> ButtonAttributes;
+        protected abstract void Click();
+
+        protected T GetParameters<T>() 
+        {
+            return JsonSerializer.Deserialize<T>(Cell.Parameters);
+        }
+
+        protected override void OnInitialized()
+        {
+            ButtonAttributes = new Dictionary<string, object>();
+            if (!string.IsNullOrEmpty(Cell.BackgroundColor))
+                ButtonAttributes.Add("style", "background-color: " + Cell.BackgroundColor);
+        }
+    }
+}
