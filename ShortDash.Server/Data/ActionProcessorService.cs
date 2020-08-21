6363a1cc764df16e79a832a2ebc9fe29730b9177
David Lambert <codepenguin@gmail.com>
2020-08-20 21:23:42 -0400

    Moved action execution out of the button.

---
+++ ShortDash.Server/Data/ActionProcessorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShortDash.Server.Data
+{
+    public class ActionProcessorService
+    {
+        // TODO: Change actionId to a GUID?
+        public void Execute(string actionId, string parameters, bool toggleState)
+        {
+            Console.WriteLine($"Clicked {actionId} - {toggleState} - {parameters}");
+        }
+
+    }
+}
--- ShortDash.Server/Startup.cs
+++ ShortDash.Server/Startup.cs
@@ -41,6 +41,7 @@ namespace ShortDash.Server
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<DashboardModelService>();
+            services.AddSingleton<ActionProcessorService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
