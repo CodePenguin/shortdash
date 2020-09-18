da33bd847f784df2b2838d1b319a92d085af9708
David Lambert <codepenguin@gmail.com>
2020-09-18 14:17:35 -0400

    Basic AES encryption handling

---
+++ ShortDash.Core/Services/EncryptedCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace ShortDash.Core.Services
+{
+    public struct EncryptedCommand
+    {
+        public EncryptedCommand(string data, string signature)
+        {
+            Data = data;
+            Signature = signature;
+        }
+
+        public string Data { get; }
+        public string Signature { get; }
+    }
+}
